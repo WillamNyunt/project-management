ce84bb20ca40e0aedf5d97dfc2de1a129dd05366
WillamNyunt <williamnyunt@outlook.com>
2024-03-22 15:07:44 +0000

    styling project task modal

--- src/ProjectTaskModal.jsx
+++ src/ProjectTaskModal.jsx
@@ -4,16 +4,20 @@ import Modal from './Modal'
 export default function ProjectTaskModal({ project, onProjectDelete }) {
     return (
         <Modal>
-            <div>
+            <div className="flex flex-col gap-y-5">
                 <div>
                     {project.title}
                     <button onClick={() => onProjectDelete(project)}>Delete</button>
                 </div>
-                {project.date}
-                {project.description}
+                <p>{project.date}</p>
+                <p>{project.comment}</p>
             </div>
             <hr/>
             <h2>Tasks</h2>
+            <div className='flex flex-row justify-between'>
+                <input type='text' className='flex-grow' />
+                <button>Add task</button>
+            </div>
         </Modal>
     )
 }
